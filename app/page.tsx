72331307ec797796c4e354c6c61653e9b3705da6
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-03-02 17:31:23 -0500

    style: changed the landing page default text

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 px-6 md:px-0">
       <div className="text-3xl font-bold md:text-7xl">{`<Coming Soon />`}</div>
       <p className="max-w-4xl text-center text-2xl md:text-start">
-        A website where developers can share their uses: Desk Setups, Streaming
-        HW, Coding Environments, productivity SW, and more!
+        A website for software developers to share their uses: Desk Setups,
+        Streaming HW, Coding Environments, productivity SW, and more!
       </p>
     </main>
   )
